938873e6310d63a3d0904991f372fabf5455a217
Himanshu <Himanshusharma.cse23@jecrc.ac.in>
2023-08-17 19:25:58 +0530

    commit added cards

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,14 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             title: 'Portfolio',
             themeMode: provider.currentTheme(),
-            darkTheme: ThemeData.dark(useMaterial3: true),
-            theme: ThemeData.light(useMaterial3: true),
+            darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
+                textTheme: TextTheme(bodyLarge: TextStyle(color: Colors.black)),
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87),
+                cardTheme: CardTheme(
+                    color: Colors.white54, surfaceTintColor: Colors.brown)),
+            theme: ThemeData.light(useMaterial3: true).copyWith(
+                cardTheme: CardTheme(
+                    color: Colors.black54, surfaceTintColor: Colors.grey)),
             // theme: ThemeData.dark(useMaterial3: true),
             home: child, debugShowCheckedModeBanner: false,
           );
